184b4685f42b3e9b64a6c8bc8fc8aed560d149af
Sugam Garg <sugam110795@gmail.com>
2025-11-15 11:27:34 -0500

    Initial Commit

---
+++ Image2Calendar/Image2CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image2CalendarApp.swift
+//  Image2Calendar
+//
+//  Created by Sugam Garg on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Image2CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
